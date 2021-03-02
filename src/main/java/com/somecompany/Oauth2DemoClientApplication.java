a74afc1150063f04ad2388eca9b2dc1f8d3faf37
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-02 11:59:38 +1100

    Setup initial Spring Boot skeleton.

---
+++ src/main/java/com/somecompany/Oauth2DemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.somecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2DemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2DemoClientApplication.class, args);
+	}
+
+}
