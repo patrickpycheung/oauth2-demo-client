d77ed71b30a2cf67bd5c9feae6be84435ad9da17
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-04-01 19:26:10 +1100

    Use ClientRegistrationRepository instead of ReactiveClientRegistrationRepository.

--- src/main/java/com/somecompany/configuration/WebClientConfig.java
+++ src/main/java/com/somecompany/configuration/WebClientConfig.java
@@ -2,23 +2,24 @@ package com.somecompany.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
-	@Bean
-	WebClient webClient(ReactiveClientRegistrationRepository clientRegistrations,
-			ReactiveOAuth2AuthorizedClientService authorizedClientService) {
-		ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(
-				new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrations,
-						authorizedClientService));
-		oauth.setDefaultOAuth2AuthorizedClient(true);
-		oauth.setDefaultClientRegistrationId("okta");
-		return WebClient.builder().filter(oauth).build();
-	}
+    @Bean
+    WebClient webClient(
+            ClientRegistrationRepository clientRegistrations, OAuth2AuthorizedClientRepository authorizedClients
+    ) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrations, authorizedClients);
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
+        oauth2.setDefaultClientRegistrationId("okta");
+        return WebClient.builder()
+                .apply(oauth2.oauth2Configuration())
+                .build();
+    }
 }
